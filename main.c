5766108108dc976ddda4c8f05e74a1cb46c5a7ba
andientor <andientor@memeware.net>
2020-08-28 21:23:26 -0600

    Title Method

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 #include <stdint.h>
 
+void printTitle(){
+	printf("\n----------------------------\n");
+	printf("  Welcome to online chess!  \n");
+	printf("----------------------------\n\n");
+
+	printf("What do you want to do? [Enter number to progress]\n");
+	printf("1. Play locally vs AI\n");
+	printf("2. Play locally vs Human\n");
+	printf("3. Play online\n\n");
+	printf("Enter your choice: ");
+}
+
 int main(){
-	printf("♕");
+	printTitle();
+
+	return 0;
 }
