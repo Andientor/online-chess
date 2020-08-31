9aad0a9acad629becc22746aa6e52d8f2a3597eb
andientor <andientor@memeware.net>
2020-08-30 20:18:24 -0600

    Chess engine header

---
+++ engine.h
@@ -0,0 +1,7 @@
+#pragma once
+
+int8_t findChecks(int8_t board[8][8], int8_t coords[4]);
+
+int8_t validateMove(int8_t board[8][8], int8_t coords[4]);
+
+void cpuMove(int8_t board[8][8], int8_t coords[4]);
